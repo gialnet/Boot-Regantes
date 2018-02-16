ef782a3c1e620a92b8f5f543bb014666c5d0637b
Antonio Perez Caballero <antonio.gialnet@gmail.com>
2018-02-16 11:17:33 +0000

    README improve

--- src/main/java/com/gialnet/bootregantes/estanques/EstanquesRepository.java
+++ src/main/java/com/gialnet/bootregantes/estanques/EstanquesRepository.java
@@ -1,9 +1,11 @@
 package com.gialnet.bootregantes.estanques;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface EstanquesRepository extends CrudRepository<Estanques, String> {
 
     List<Estanques> findAll();
